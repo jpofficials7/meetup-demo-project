4bbf52a505c90eb70f5a03f3588760141a2e838a
jayant pal <jpofficials7@gmail.com>
2023-08-18 18:32:35 +0530

    adding a layout component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   //Domain: localhost:3000/
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route exact path="/" element={<AllMeetupsPage />} />
         <Route exact path="/new-meetup" element={<NewMeetupPage />} />
         <Route exact path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
