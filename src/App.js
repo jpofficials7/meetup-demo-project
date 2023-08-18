32a5a67c2dea6f951daeb4c0c2c284a0b9b39063
jayant pal <jpofficials7@gmail.com>
2023-08-18 15:51:13 +0530

    added Main Navigation component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   //Domain: localhost:3000/
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route exact path="/" element={<AllMeetupsPage />} />
         <Route exact path="/new-meetup" element={<NewMeetupPage />} />
