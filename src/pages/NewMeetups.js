d6f78cfec785b4503ca1aef7847a95986b1822c4
jayant pal <jpofficials7@gmail.com>
2023-08-18 19:59:32 +0530

    using useNavigate  to handle form submission

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://meetup-demo-backend-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetupPage() {
           "content-type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      navigate("/");
+    });
   }
   return (
     <section>
